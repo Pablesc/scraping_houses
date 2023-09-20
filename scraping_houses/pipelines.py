4ba5b6b13b0dba8b3c45a554a3646c2a6d603a01
Pablo Cortés <pablocortes1991@gmail.com>
2023-09-20 12:37:53 -0300

    Drop test collection

--- scraping_houses/pipelines.py
+++ scraping_houses/pipelines.py
@@ -14,14 +14,12 @@ class ScrapingHousesPipeline:
         self.db_departamentos = self.conn['Data_departamentos']
         self.collection_ventas_departamentos = self.db_departamentos['Ventas']
         self.collection_arriendo_departamentos = self.db_departamentos['Arriendo']
-        self.collection_datos_faltantes = self.db_departamentos['Datos faltantes3']
     def process_item(self, item, spider):
         if item['tipo'] == 'venta':
             if item['propiedad'] == 'casa':
                 self.collection_ventas_casas.insert_one(dict(item))
             elif item['propiedad'] == 'departamento':
-            #    self.collection_ventas_departamentos.insert_one(dict(item))
-                self.collection_datos_faltantes.insert_one(dict(item))
+                self.collection_ventas_departamentos.insert_one(dict(item))
         elif item['tipo'] == 'arriendo':
             if item['propiedad'] == 'casa':
                 self.collection_arriendo_casas.insert_one(dict(item))
